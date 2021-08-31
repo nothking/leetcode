ab3f9bb46f8ab8a369584ca070dbe88059b25957
chenli <king307472360@gmail.com>
2021-08-31 21:44:05 +0800

    1109

--- nothingtest/test01.java
+++ nothingtest/test01.java
@@ -6,23 +6,28 @@ public class test01 {
     public static void main(String[] args) throws InterruptedException {
         Random r = new Random();
         test01 t = new test01();
-        System.out.println(t.generate(25));
+        for(int i = 0;i < 10; i ++ ){
+            System.out.println(t.generate(25));
+        }
     }
 
     String generate(int n) {
-        char[] tem = new char[52];
+        char[] tem = new char[62];
         int idx = 0;
         for (int i = 0; i < 26; i++) {
             tem[idx++] = (char) ('a' + i);
             tem[idx++] = (char) ('A' + i);
         }
+        for(int i = 0 ; i < 10 ;i++){
+            tem[idx++] =(char)('0'+ i);
+        }
         Random r = new Random();
         char[] result = new char[n];
         idx = 0;
         while (idx < n) {
-            char c = tem[r.nextInt(51)];
+            char c = tem[r.nextInt(62)];
             while (idx != 0 && c == result[idx - 1]) {
-                c = tem[r.nextInt(51)];
+                c = tem[r.nextInt(62)];
             }
             result[idx++] = c;
         }
