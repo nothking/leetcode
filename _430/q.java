6e3bea1d999abbe725c53d390d95022d56ef5269
nothking <king520025@foxmail.com>
2021-09-24 22:27:40 +0800

    copy

---
+++ _430/q.java
@@ -0,0 +1,43 @@
+package _430;
+
+import java.util.*;
+import java.util.Stack;
+
+public class q {
+    public static void main(String[] args) {
+
+    }
+
+    
+    
+    public Node flatten(Node head) {
+        if(head==null)return null;
+        if(head.child==null){
+            flatten(head.next);
+            return head;
+        }
+        Node first=flatten(head.child);
+        Node second = flatten(head.next);
+        head.next=first;
+        first.prev=head;
+        while(first.next!=null){
+            first=first.next;
+        }
+        first.next=second;
+        if(second!=null)
+            second.prev=first;
+
+        head.child=null;    
+        return head;
+    }
+   
+
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    };
+
+}
