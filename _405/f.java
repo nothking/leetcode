7e74c3059f787ae8654cb8fbe5b7508a7d02a602
nothking <king520025@foxmail.com>
2021-10-02 18:54:26 +0800

    false

---
+++ _405/f.java
@@ -0,0 +1,10 @@
+package _405;
+
+public class f {
+    public static void main(String[] args){
+        
+    }
+    public String toHex(int num) {
+        return Integer.toHexString(num);
+    }
+}
