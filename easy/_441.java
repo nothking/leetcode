98a469ccfee51b6ed2407bef73128f705208811e
nothking <king520025@foxmail.com>
2021-10-10 12:52:15 +0800

    1010easy

---
+++ easy/_441.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class _441 {
+    public static void main(String[] args) {
+        System.out.println(arrangeCoins(2));
+    }
+
+    public static int arrangeCoins(int n) {
+        int i = 1;
+        int count = 0;
+        while (count <= n-i){
+            count += i;
+            ++i;
+        }
+        return i-1;
+    }
+}
