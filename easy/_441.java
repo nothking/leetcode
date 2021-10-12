e561c55f374748a6bcc782359672a773b7c9a214
nothking <king520025@foxmail.com>
2021-10-12 21:34:35 +0800

    test font

--- easy/_441.java
+++ easy/_441.java
@@ -3,6 +3,7 @@ package easy;
 public class _441 {
     public static void main(String[] args) {
         System.out.println(arrangeCoins(2));
+        //test font RI
     }
 
     public static int arrangeCoins(int n) {
